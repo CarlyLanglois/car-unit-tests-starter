ac784499960dfd895b416ed7756be10992b14de1
Blake Mills <welzie@gmail.com>
2017-12-21 13:34:30 -0600

    the start of the starter

---
+++ src/test/org/launchcode/training/CarTest.java
@@ -0,0 +1,12 @@
+package org.launchcode.training;
+
+
+
+public class CarTest {
+
+    //TODO: constructor sets gasTankLevel properly
+    //TODO: gasTankLevel is accurate after driving within tank range
+    //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    //TODO: can't have more gas than tank size, expect an exception
+
+}
